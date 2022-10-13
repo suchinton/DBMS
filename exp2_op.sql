8404b058f5adf0c7a1eae44f9bd280931f7a8267
suchinton <75079303+suchinton@users.noreply.github.com>
2022-10-13 19:06:32 +0530

    .

---
+++ exp2_op.sql
@@ -0,0 +1,60 @@
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: exp2
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Suchinton`
+--
+
+DROP TABLE IF EXISTS `Suchinton`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Suchinton` (
+  `CourseName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CourseNo` int(11) DEFAULT NULL,
+  `CreditHours` int(11) DEFAULT NULL,
+  `Deptartment` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Year` int(11) DEFAULT NULL,
+  `FACULTY_NAME` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Branch` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Suchinton`
+--
+
+LOCK TABLES `Suchinton` WRITE;
+/*!40000 ALTER TABLE `Suchinton` DISABLE KEYS */;
+INSERT INTO `Suchinton` VALUES
+('DBMS',301,2,'CSE',3,NULL,NULL),
+('EN',302,2,'CSE',3,NULL,NULL),
+('BS',303,2,'CSE',3,NULL,NULL),
+('DS',304,2,'CSE',3,NULL,NULL),
+('ML',305,2,'CSE',3,NULL,NULL);
+/*!40000 ALTER TABLE `Suchinton` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-30  0:42:26
---
+++ exp8_op.sql
@@ -0,0 +1,89 @@
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: exp8
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Suchinton`
+--
+
+DROP TABLE IF EXISTS `Suchinton`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Suchinton` (
+  `CourseName` varchar(255) DEFAULT NULL,
+  `CourseNo` int(11) DEFAULT NULL,
+  `CreditHours` int(11) DEFAULT NULL,
+  `Deptartment` varchar(255) DEFAULT NULL,
+  `Semester` int(11) DEFAULT NULL,
+  `Year` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Suchinton`
+--
+
+LOCK TABLES `Suchinton` WRITE;
+/*!40000 ALTER TABLE `Suchinton` DISABLE KEYS */;
+INSERT INTO `Suchinton` VALUES
+('DBMS',301,2,'CSE',5,3),
+('EN',302,3,'CSE',5,3),
+('BS',303,2,'CSE',5,3),
+('DS',304,4,'CSE',5,3),
+('ML',305,5,'CSE',5,3);
+/*!40000 ALTER TABLE `Suchinton` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faculty`
+--
+
+DROP TABLE IF EXISTS `faculty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faculty` (
+  `Id` int(11) NOT NULL,
+  `Full_Name` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CourseName` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faculty`
+--
+
+LOCK TABLES `faculty` WRITE;
+/*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
+INSERT INTO `faculty` VALUES
+(131,'Mr. Sunil','ML'),
+(132,'Mrs. Renuka','DS'),
+(135,'Mrs. Neha','BS'),
+(136,'Mr. Sunil','EN'),
+(138,'Mr. Gupta','DBMS');
+/*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-13 19:03:22
